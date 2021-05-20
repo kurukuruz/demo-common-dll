2a6932b7328589729669d00672b426f138d47b3e
kurukuruz <kurukuruz.4dev@gmail.com>
2021-05-20 19:06:08 +0900

    DLLを呼び出す処理を追加

--- csharp/src/demo-csharp.cs
+++ csharp/src/demo-csharp.cs
@@ -1,9 +1,18 @@
 using System;
+using System.Runtime.InteropServices;
+using System.Text;
 
 namespace DemoApp {
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello, world.");
+            StringBuilder outBuf = new StringBuilder(256);
+            NativeMethods.my_func_a("hogehoge", outBuf);
+            Console.WriteLine(outBuf.ToString());
         }
+    }
+
+    static class NativeMethods {
+        [DllImport("..\\..\\common-dll\\common-dll\\dest\\my_common_lib.dll", CallingConvention = CallingConvention.StdCall)]
+        public extern static bool my_func_a(string in_str, StringBuilder out_str);
     }
 }
